7845ef4fe27d512247004542aae868046ef88c06
martintrochez <trochezmartin@gmail.com>
2026-03-11 00:47:58 +0000

    Lesson 1

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
+	"mime"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +31,71 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Could not find the video", err)
+		return
+	}
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can't access this video", nil)
+		return
+	}
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not parse form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not get thumbnail file", err)
+		return
+	}
+	defer file.Close()
+
+
+	mediaType := header.Header.Get("Content-Type")
+	mimeType, _, err := mime.ParseMediaType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error getting mimeType", err)
+		return
+	}
+
+	if mimeType != "image/png" && mimeType != "image/jpeg" {
+		respondWithError(w, http.StatusForbidden, "Only jpge and png files accepted", nil)
+		return
+	}
+
+	ext := filepath.Ext(header.Filename)
+
+	fileName := video.ID.String() + ext
+
+	dstPath := filepath.Join(cfg.assetsRoot, fileName)
+
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing file", err)
+		return
+	}
+
+	videoThumbnailURL := filepath.Join("/assets", fileName)
+	video.ThumbnailURL = &videoThumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update the video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
